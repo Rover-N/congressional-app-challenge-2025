c77e6ef4b3b6a782bc04d449ed3dc1ec7647f9a7
Noah <Weedenenoah@gmail.com>
2025-10-24 08:55:31 -0400

    Changed how it works to look nicer

--- scripts/menu_button.gd
+++ scripts/menu_button.gd
@@ -3,6 +3,6 @@ extends Button
 signal menu_opened(buttonIndex: int)
 
 func _pressed():
-    menu_opened.emit(get_index())
-    $Screen/EventMenuShow.visible = !$Screen/EventMenuShow.visible
-    
+	menu_opened.emit(get_index())
+	$Screen/EventMenuShow.visible = !$Screen/EventMenuShow.visible
+	
